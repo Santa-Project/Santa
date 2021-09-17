bd1928deeb0b8a8f1a0a7ea7bbee6bbf520b0c8a
cambt1 <cambt@daum.net>
2021-09-17 19:37:33 +0900

    [add]main 메인.jsp 및 컨트롤러 생성_양이슬

---
+++ src/com/kh/santa/main/controller/MainController.java
@@ -0,0 +1,56 @@
+package com.kh.santa.main.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.santa.common.wrapper.RequestWrapper;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("/main/*")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestWrapper wrappedRequest = (RequestWrapper)request;
+		String[] uriArr = wrappedRequest.getRequestURIArray();
+		
+		switch(uriArr[uriArr.length-1]){
+		case "main" :
+			main(request,response);
+			break;
+		default :
+			break;
+		}
+	}
+
+	private void main(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/main/main").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
