cdb6d14d460ded1611f606b5fb14267b1241bbea
hyc20 <hyc20@naver.com>
2021-10-01 20:04:43 +0900

    [update] matching update_홍용천

--- src/com/kh/santa/matching/controller/MatchingController.java
+++ src/com/kh/santa/matching/controller/MatchingController.java
@@ -1,6 +1,8 @@
 package com.kh.santa.matching.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.santa.common.wrapper.RequestWrapper;
+import com.kh.santa.matching.model.dto.MatchingBoard;
+import com.kh.santa.matching.model.dto.OpenRoom;
+import com.kh.santa.matching.model.service.MatchingBoardService;
 
 /**
  * Servlet implementation class MatchingController
@@ -16,9 +21,8 @@ import com.kh.santa.common.wrapper.RequestWrapper;
 public class MatchingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private MatchingBoardService matchingBoardService = new MatchingBoardService();
+    
     public MatchingController() {
         super();
         // TODO Auto-generated constructor stub
@@ -35,20 +39,73 @@ public class MatchingController extends HttpServlet {
 		case "collectTeam" :
 			collectTeam(request,response);
 			break;
-		case "collectTeam_chat" :
-			collectTeam_chat(request,response);
-			break;
 		case "openRoom" :
 			openRoom(request,response);
+			break;
+		case "list" :
+			list(request,response);
+			break;
+		case "write_view" :
+			write_view(request,response);
+			break;
+		case "reply_view" :
+			reply_view(request,response);
+			break;
+		case "content_view" :
+			content_view(request,response);
+
+		case "collectTeam_chat" :
+			collectTeam_chat(request,response);
 			break;
 		default :
 			break;
 		}
 	}
 
-	private void openRoom(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		request.getRequestDispatcher("/match/openRoom").forward(request, response);
+	
+	
+	private void content_view(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
+		String mbIdx = request.getParameter("bId");
+		System.out.println("content_view가 호출되었습니다  ");
+		MatchingBoard dto = matchingBoardService.getContentView(Integer.parseInt(mbIdx));
+		request.setAttribute("content_view", dto);
+		request.getRequestDispatcher("/match/content_view").forward(request, response);
 		
+	}
+
+	private void reply_view(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
+		request.getRequestDispatcher("/match/reply_view").forward(request, response);
+		
+	}
+
+	private void write_view(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
+		System.out.println("write_view가 호출되었습니다  ");
+		request.getRequestDispatcher("/match/write_view").forward(request, response);
+		
+	
+	}
+
+	private void list(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
+		System.out.println("list가 호출되었습니다  ");
+		ArrayList<MatchingBoard> dtos = matchingBoardService.getMatchingList();
+		request.setAttribute("list", dtos);
+		request.getRequestDispatcher("/match/list").forward(request, response);
+
+	}
+
+	private void openRoom(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		MatchingBoard dto = new MatchingBoard();
+		System.out.println("openRoom가 호출되었습니다  ");
+		
+		dto.setMemberIdx(request.getParameter("memberIdx"));
+		dto.setBrdName(request.getParameter("room_title"));
+		System.out.println(request.getParameter("the_day"));
+		
+		matchingBoardService.setMatchingBoard(dto);
+		request.setAttribute("open_room",dto);
+		
+		request.getRequestDispatcher("/match/openRoom").forward(request, response);
+
 	}
 
 	private void collectTeam_chat(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
@@ -56,6 +113,7 @@ public class MatchingController extends HttpServlet {
 	}
 
 	private void collectTeam(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		
 		request.getRequestDispatcher("/match/collectTeam").forward(request, response);
 
 		
@@ -68,5 +126,6 @@ public class MatchingController extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+
 
 }
--- src/com/kh/santa/matching/model/service/MatchingBoardService.java
+++ src/com/kh/santa/matching/model/service/MatchingBoardService.java
@@ -1,13 +1,73 @@
 package com.kh.santa.matching.model.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 import com.kh.santa.common.db.JDBCTemplate;
 import com.kh.santa.matching.model.dao.MatchingBoardDao;
+import com.kh.santa.matching.model.dto.MatchingBoard;
+import com.kh.santa.matching.model.dto.OpenRoom;
 
 public class MatchingBoardService {
 
 
-	private MatchingBoardDao matchingBoardDao = new MatchingBoardDao();
+	private  MatchingBoardDao matchingBoardDao = new MatchingBoardDao();
 	private JDBCTemplate template = JDBCTemplate.getInstance();
+	
+	public ArrayList<MatchingBoard> getMatchingList() {
+		Connection conn = template.getConnection();
+		ArrayList<MatchingBoard> list = new ArrayList<MatchingBoard>();
+		try {
+			list = matchingBoardDao.list(conn);
+			
+		}finally {
+			template.close(conn);
+		}
+		
+		return list;
+	}
+
+	public MatchingBoard getContentView(int mbIdx) {
+		Connection conn = template.getConnection();
+		MatchingBoard content_view = new MatchingBoard();
+		try {
+			content_view = matchingBoardDao.selectBoardByIdx(mbIdx, conn);
+			
+		}finally {
+			template.close(conn);
+		}
+		
+		return content_view;
+	}
+
+	public ArrayList<OpenRoom> getopenRoom() {
+		Connection conn = template.getConnection();
+		ArrayList<OpenRoom> open_room = new ArrayList<OpenRoom>(); 
+		try {
+			open_room = matchingBoardDao.roomContent(conn);
+			
+		}finally {
+			template.close(conn);
+		}
+		
+		return open_room;
+	}
+
+	public void setMatchingBoard(MatchingBoard dto) {
+		Connection conn = template.getConnection();
+		try {
+			/* matchingBoardDao.insertMatchingBoard(dto,conn); */
+			template.commit(conn);
+		}catch(Exception e){
+			template.rollback(conn);
+			throw e;
+		}finally {
+			template.close(conn);
+		}
+		
+	}
+
+
 	
 	
 }
